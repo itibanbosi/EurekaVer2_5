a9efa85c65ce374d00dfb31067b7adbbdc7435f6
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-07-29 08:16:13 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -42,11 +42,15 @@ let con_le = 0;
 let con_op = 0;
 
 //% color="#74ad1d" block="ﾕｰﾚｶﾌﾞﾛｯｸ2.4"
+
+  namespace eureka_blocks {
+  //% color="#ff3d03" weight=59 blockId=eureka_buz_set block="ﾕｰﾚｶIOﾎﾞｯｸｽで音をならす" group="1_初期設定"
+
     /**
      * micro:bit本体の明るさセンサーが暗い場合（20未満）に真を返します。
      */
-  namespace eureka_blocks {
-  //% color="#ff3d03" weight=59 blockId=eureka_buz_set block="ﾕｰﾚｶIOﾎﾞｯｸｽで音をならす" group="1_初期設定"
+
+
   export function eureka_buz_set() {
     pins.analogSetPitchPin(AnalogPin.P8);
   }
